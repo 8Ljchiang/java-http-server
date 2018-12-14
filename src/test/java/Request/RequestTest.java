9dd9e69793b8fc176f1cd906c3499dcab99d2d4f
Jonathan C <jchiang@8thlight.com>
2018-12-14 12:27:08 -0600

    created RequestTest

---
+++ src/test/java/Request/RequestTest.java
@@ -0,0 +1,41 @@
+package Request;
+
+import org.junit.Test;
+
+public class RequestTest {
+
+    @Test
+    public final void testGetType() {
+
+    }
+
+    @Test
+    public final void testGetMethod() {
+
+    }
+
+    @Test
+    public final void testGetPath() {
+
+    }
+
+    @Test
+    public final void testGetHeaders() {
+
+    }
+
+    @Test
+    public final void testGetBody() {
+
+    }
+
+    @Test
+    public final void testGetRequestString() {
+
+    }
+
+    @Test
+    public final void testIsValid() {
+
+    }
+}
