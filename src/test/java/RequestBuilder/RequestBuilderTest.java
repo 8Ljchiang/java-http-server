63fc94329f1db5c033cf41750dd374758b3c9be7
Jonathan C <jchiang@8thlight.com>
2018-12-14 16:19:50 -0600

    fixed tests

--- src/test/java/RequestBuilder/RequestBuilderTest.java
+++ src/test/java/RequestBuilder/RequestBuilderTest.java
@@ -18,6 +18,13 @@ public class RequestBuilderTest {
     String requestString = "";
     HashMap<String, String> headers = new HashMap<>();
 
+    Request request;
+
+    @Before
+    public final void setup() {
+        request = RequestBuilder.createRequest(requestString);
+    }
+
     @Test
     public final void testCreateRequest() {
         Request request = RequestBuilder.createRequest(requestString);
@@ -41,12 +48,14 @@ public class RequestBuilderTest {
 
     @Test
     public final void testGetFirstLine() {
-        assertEquals(method + " " + path + " " + type + "\n", request.getFirstLine());
+        String[] requestStringArray = requestString.split("\n");
+        assertEquals(method + " " + path + " " + type + "\n", RequestBuilder.getFirstLine(requestStringArray));
     }
 
     @Test
     public final void testGetHeaders() {
-        HashMap<String, String> requestHeaders = request.getHeaders();
+        String[] requestStringArray = requestString.split("\n");
+        HashMap<String, String> requestHeaders = RequestBuilder.getHeaders(requestStringArray);
 
         for (HashMap.Entry<String, String> entry : headers.entrySet()) {
             String key = entry.getKey();
@@ -59,6 +68,31 @@ public class RequestBuilderTest {
 
     @Test
     public final void testGetBody() {
-        assertEquals(body, request.getBody());
+        String[] requestStringArray = requestString.split("\n");
+        assertEquals(body, RequestBuilder.getBody(requestStringArray));
+    }
+
+    @Test
+    public final void testIsValidRequestString() {
+        String validRequestString = requestString;
+        assertTrue(RequestBuilder.isValidRequestString(validRequestString));
+
+        String invalidRequestString1 = "";
+        assertFalse(RequestBuilder.isValidRequestString(invalidRequestString1));
+
+        String invalidRequestString2 = "GET";
+        assertFalse(RequestBuilder.isValidRequestString(invalidRequestString2));
+
+        String invalidRequestString3 = "GET /home";
+        assertFalse(RequestBuilder.isValidRequestString(invalidRequestString3));
+
+        String invalidRequestString4 = "GET /home HTTP/1.1";
+        assertFalse(RequestBuilder.isValidRequestString(invalidRequestString4));
+
+        String invalidRequestString5 = "ASDF /home HTTP/1.1";
+        assertFalse(RequestBuilder.isValidRequestString(invalidRequestString5));
+
+        String invalidRequestString6 = "GET /home ASDF/1.1";
+        assertFalse(RequestBuilder.isValidRequestString(invalidRequestString6));
     }
 }
