fb1ad4d365788f1e57db06b6570d1101a1a927bb
Jonathan C <jchiang@8thlight.com>
2018-12-14 11:28:35 -0600

    initial test for RouteController

---
+++ src/test/java/RouteController/RouteControllerTest.java
@@ -0,0 +1,37 @@
+package RouteController;
+
+import hello.Greeter;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class RouteControllerTest {
+    @Test
+    public void handleCallsLambdaOperation() {
+
+        String methodCallString = "mockControllerLambda";
+        ArrayList<String> methodCalls = new ArrayList<>();
+
+        // 0. Create a mock lambda function for the controller constructor.
+        IRouteControllerLambda mockControllerLambda = (IRequest req, IResponse res) -> {
+            methodCalls.add(methodCallString);
+        };
+
+        // 1. Initialize the Route Controller.
+        RouteController routeController = new RouteController(mockControllerLambda);
+
+        // 2. Create mock Request and Reseponse objects to pass into the handle function.
+        IRequest mockRequest = new RequestMock();
+        IResponse mockResponse = new ResponseMock();
+
+        // 3. Call RouteController handle method.
+        routeController.handle(mockRequest, mockResponse);
+
+        // 4. Assert that the lambda method was called once.
+
+        // assert statements
+        assertEquals(true, methodCalls.contains(methodCallString));
+    }
+}
