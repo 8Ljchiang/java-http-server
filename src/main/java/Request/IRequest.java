1da40d88bd4cdd349b2caf4f9d5e4b4a22fac881
Jonathan C <jchiang@8thlight.com>
2018-12-14 11:35:46 -0600

    created IRequest

---
+++ src/main/java/Request/IRequest.java
@@ -0,0 +1,14 @@
+package Request;
+
+import java.util.HashMap;
+
+public interface IRequest {
+    public String getType();
+    public String getPath();
+    public String getMethod();
+    public String getBody();
+    public HashMap<String, String> getHeaders();
+    public String getHeader(String headerName);
+    public String getRequestString();
+    public Boolean isValid();
+}
