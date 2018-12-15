d7b88acdf8d5b9eb5529df89a4631023814a4ead
Jonathan C <jchiang@8thlight.com>
2018-12-15 06:59:14 -0600

    created initial Request test

---
+++ src/test/java/Response/ResponseTest.java
@@ -0,0 +1,34 @@
+package Response;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class ResponseTest {
+
+    @Test
+    public final void testSetStatus() {
+
+    }
+
+    @Test
+    public final void testSetContentType() {
+
+    }
+
+    @Test
+    public final void testAddHeader() {
+
+    }
+
+    @Test
+    public final void testSetBody() {
+
+    }
+
+
+    @Test
+    public final void testSend() {
+        
+    }
+
+}
