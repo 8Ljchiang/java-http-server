1eb25e9102edb22c76016ab6bcddb8fb798628ff
Jonathan C <jchiang@8thlight.com>
2018-12-15 07:10:54 -0600

    added setStatus test

--- src/test/java/Response/ResponseTest.java
+++ src/test/java/Response/ResponseTest.java
@@ -1,13 +1,35 @@
 package Response;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.lang.reflect.Field;
+
 import static org.junit.Assert.assertEquals;
 
 public class ResponseTest {
 
+    public class SocketConnectionMock implements ISocketConnection {
+
+    }
+
+    private SocketConnectionMock socketConnection = new SocketConnectionMock();
+    private Response response;
+
+    @Before
+    public final void setup() {
+        response = new Response(socketConnection);
+    }
+
     @Test
     public final void testSetStatus() {
+        String notFoundStatus = "404 Not Found";
+        response.setStatus(notFoundStatus);
 
+        //then
+        final Field field = response.getClass().getDeclaredField("status");
+        field.setAccessible(true);
+        assertEquals("Fields didn't match", field.get(response), notFoundStatus);
     }
 
     @Test
@@ -25,10 +47,9 @@ public class ResponseTest {
 
     }
 
-
     @Test
     public final void testSend() {
-        
+
     }
 
 }
