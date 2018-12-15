9af4c3d33ed436482e92034fb571b0e210115f5b
Jonathan C <jchiang@8thlight.com>
2018-12-15 08:18:28 -0600

    created Response class

---
+++ src/main/java/Response/Response.java
@@ -0,0 +1,37 @@
+package Response;
+
+import java.util.HashMap;
+
+public class Response {
+    private ISocketConnection sc;
+    private String status = "200 OK";
+    private String http = "HTTP/1.1";
+    private String type = "text/html";
+    private HashMap<String, String> headers = new HashMap<>();
+
+    public Response(ISocketConnection sc) {
+        this.sc = sc;
+        this.headers.put("Content-Type", type);
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() { return this.status; }
+
+    public void addHeader(String key, String value) {
+        this.headers.put(key, value);
+    }
+
+    public HashMap<String, String> getHeaders() { return this.headers; }
+
+    public void setContentType(String type) {
+        this.type = type;
+        this.headers.put("Content-Type", type);
+    }
+
+    public String getContentType() {
+        return this.headers.get("Content-Type");
+    }
+}
