29c682ced1559570a3d5404438481c58e221c779
Jonathan C <jchiang@8thlight.com>
2018-12-15 15:21:55 -0600

    added use method test

--- src/test/java/Router/RouterTest.java
+++ src/test/java/Router/RouterTest.java
@@ -28,8 +28,41 @@ public class RouterTest {
     }
 
     @Test
-    public final void testUse() throws IllegalAccessException {
+    public final void testUse() throws IllegalAccessException, NoSuchFieldException {
+        String path = "/";
+        String expectedMethod = Methods.GET.toString();
+        int expectedNumOfPaths = 1;
+        int expectedNumOfRoutes = 1;
 
+        // Test the creation of a GET route and added to the router.
+        IRouteControllerLambda defaultControllerLambda = (IRequest req, IResponse res) -> {
+            return res;
+        };
+
+        RouteController defaultController = new RouteController(defaultControllerLambda);
+        router.use(expectedMethod, path, defaultController);
+
+        // Get HashMap of Routes
+
+        final Field field = router.getClass().getDeclaredField("routes");
+        field.setAccessible(true);
+        HashMap<String, ArrayList<Route>> routerRoutes = (HashMap<String, ArrayList<Route>>)field.get(router);
+
+        // Check for proper number of keys.
+        int numOfPaths = routerRoutes.size();
+        assertEquals(expectedNumOfPaths, numOfPaths);
+
+        // Check that router paths all exist.
+        assertTrue(routerRoutes.containsKey(path));
+
+        // Check for proper number of routes on path (key).
+        int numOfRoutes = routerRoutes.get(path).size();
+        assertEquals(expectedNumOfRoutes, numOfRoutes);
+
+        // Check that route has proper fields.
+        Route resultRoute = routerRoutes.get(path).get(0);
+        assertEquals(path, resultRoute.getPath());
+        assertEquals(expectedMethod, resultRoute.getMethod());
     }
 
     @Test
