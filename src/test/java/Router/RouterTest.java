2911ed53324d613f4c7508a3457b8c5f299dbde6
Jonathan C <jchiang@8thlight.com>
2018-12-15 14:34:49 -0600

    created RouterTest setup

---
+++ src/test/java/Router/RouterTest.java
@@ -0,0 +1,45 @@
+package Router;
+
+import Request.IRequest;
+import Response.IResponse;
+import RouteController.IRouteController;
+import RouteController.IRouteControllerLambda;
+import RouteController.RouteController;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class RouterTest {
+
+    private Router router;
+
+    @Before
+    public final void setup() {
+        Router router = new Router();
+    }
+
+    @Test
+    public final void testGet() {
+
+    }
+
+    @Test
+    public final void testPut() {
+
+    }
+
+    @Test
+    public final void testPost() {
+
+    }
+
+    @Test
+    public final void testDelete() {
+
+    }
+
+    @Test
+    public final void testHandleRequest() {
+
+    }
+}
